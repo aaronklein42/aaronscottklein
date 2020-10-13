cd397b16249fcfaa71c674b899bd289714faad14
Aaron Klein <aaronklein42@gmail.com>
2020-10-13 19:20:17 -0400

    padding bottom

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,27 @@
 import Content from 'components/content/content'
 import PageContent from 'components/pageContent/pageContent'
 
+const projects = [
+	{
+  	name: 'Tic Tac Toe',
+    url: '/projects/tic-tac-toe',
+    thumbnail: 'tic-tac-toe.jpg'
+  },
+  {
+  	name: 'Shrinking Space',
+    url: '/projects/shrinking-space',
+    thumbnail: 'shrinking-space.jpg'
+  },
+ 	{
+  	name: 'Traveler',
+    url: '/projects/traveler',
+    thumbnail: 'traveler.jpg'
+  }
+]
+
+projects.forEach((project) => {
+	console.log(project);
+});
 
 export default function Projects() {
   return (
